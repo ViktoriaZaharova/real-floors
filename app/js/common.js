d13112bd6082e85a05c65cfca9268b159065e246
viktoriazaharova <zaharova181114@gmail.com>
2020-10-05 14:50:19 +0300

    edit

--- app/js/common.js
+++ app/js/common.js
@@ -56,6 +56,7 @@ $('.staff-slider').slick({
 $('.reviews-slider').slick({
     slidesToShow: 3,
     variableWidth: true,
+    dots: true,
     prevArrow: '<button type="button" class="slick-prev"></button>',
     nextArrow: '<button type="button" class="slick-next"></button>',
     appendArrows: '.reviews-slider__nav',
@@ -65,9 +66,11 @@ $('.reviews-slider').slick({
 $('.popular-category-slider').slick({
     slidesToShow: 4,
     variableWidth: true,
+    dots: true,
     prevArrow: '<button type="button" class="slick-prev"></button>',
     nextArrow: '<button type="button" class="slick-next"></button>',
     appendArrows: '.popular-category-slider__nav',
+    appendDots: '.popular-category-slider__dots'
 });
 
 $('.article-slider').slick({
